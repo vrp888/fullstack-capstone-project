ddb113c833b42c97138520b270e95ed5a62480cb
Vorapol Rinsatitnon <vorapol.r@outlook.com>
2024-09-03 10:43:05 -0400

    Create the MainPage and Navbar

--- giftlink-backend/models/db.js
+++ giftlink-backend/models/db.js
@@ -5,13 +5,9 @@ const MongoClient = require('mongodb').MongoClient;
 // MongoDB connection URL with authentication options
 let url = `${process.env.MONGO_URL}`;
 
-let dbInstance = null;
 const dbName = "giftdb";
 
 async function connectToDatabase() {
-    if (dbInstance){
-        return dbInstance
-    };
 
     const client = new MongoClient(url);      
 
